8d637e5b786990f84ae7e5583ae96cc45a2b3430
Nicolás Ortolá <nicolas.ortola@bpmx.com.ar>
2020-09-14 23:07:28 -0300

    DaoCallback extends Model

--- app/src/main/java/com/example/markhunters/dao/Dao.java
+++ app/src/main/java/com/example/markhunters/dao/Dao.java
@@ -8,7 +8,7 @@ import org.jetbrains.annotations.Nullable;
 
 public abstract class Dao <T extends Model> {
 
-    public  void persist (@NotNull final T model, DaoCallback<T> callback) {
+    public void persist (@NotNull final T model, DaoCallback<T> callback) {
         find(model.getKey(), new DaoCallback<T>() {
             @Override
             public void onCallback(@Nullable final T persisted) {
@@ -21,7 +21,7 @@ public abstract class Dao <T extends Model> {
         });
     }
 
-    public abstract void find(@NotNull final String key, DaoCallback<T> callback);
+    public abstract void find (@NotNull final String key, DaoCallback<T> callback);
     protected abstract void create (@NotNull final T model, DaoCallback<T> callback);
     protected abstract void update (@NotNull final T model, DaoCallback<T> callback);
 }
--- app/src/main/java/com/example/markhunters/dao/DaoCallback.java
+++ app/src/main/java/com/example/markhunters/dao/DaoCallback.java
@@ -1,7 +1,9 @@
 package com.example.markhunters.dao;
 
+import com.example.markhunters.model.Model;
+
 import org.jetbrains.annotations.Nullable;
 
-public interface DaoCallback <T> {
+public interface DaoCallback <T extends Model> {
     void onCallback(@Nullable final T model);
 }
